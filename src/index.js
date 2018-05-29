305caa032abdce6fb091cc29b70b77d026b5c36b
Miluh <maytacasas21@gmail.com>
2018-05-29 18:14:03 -0500

    2da version solo js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 //Primero obtengo el input text y el input button a través de dos variables diferentes
-let string1 = document.getElementById("textToCipher");
-let offsetNumber1 = document.getElementById("number1");
+let string1 = document.getElementById("textToCipherAndDecipher");
+let offsetNumber = document.getElementById("number");
+
 let button1 = document.getElementById("cipherButton");
-//Ahora obtenemos el identificador para la respuesta del mensaje cifrado
-let result1 = document.getElementById("cipherResult");
-
-//Ahora indico que función quiero que se corra al realizar el click en el boton
-button1.addEventListener("click", cipher.encode); 
-
-
-let string2 = document.getElementById("textToDecipher");
-let offsetNumber2 = document.getElementById("number2")
 let button2 = document.getElementById("decipherButton");
-let result2 = document.getElementById("decipherResult")
 
+//Ahora obtenemos el identificador para la respuesta del mensaje cifrado y descifrado
+let result1 = document.getElementById("cipherResult");
+button1.addEventListener("click", cipher.encode); 
+let result2 = document.getElementById("decipherResult")
 button2.addEventListener("click", cipher.decode); 
+
+
+
+
+
+
