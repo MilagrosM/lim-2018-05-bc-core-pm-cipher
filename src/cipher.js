c686bb7a9216b17b6dd13a9745ac4228ae11e1b1
Miluh <maytacasas21@gmail.com>
2018-05-31 18:40:22 -0500

    3ra version

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,5 @@
 window.cipher = {
   encode: function(offset,string){
-  string = string1.value.toUpperCase();
-  offset = parseInt(offsetNumber.value);
   
   let cipherArray = [];
   
@@ -13,19 +11,18 @@ window.cipher = {
       c_secondAsciiCode = (c_firstAsciiCode - 65 + offset) % 26 + 65;
       c_newString = String.fromCharCode(c_secondAsciiCode);
       cipherArray.push(c_newString);
-      } else if(c_firstAsciiCode = 32){
+      } else {
       c_secondAsciiCode = c_firstAsciiCode;
       c_newString = String.fromCharCode(c_secondAsciiCode);
       cipherArray.push(c_newString);
      }     
   }
-  return result1.innerHTML = cipherArray.join('')
+  return cipherArray.join('');
   },
 
 
   decode: function(offset,string){
-  string = string1.value.toUpperCase();
-  offset = parseInt(offsetNumber.value);
+ 
 
   let decipherArray = [];
   
@@ -35,18 +32,21 @@ window.cipher = {
      let d_secondAsciiCode;
      let d_newString;
      if(d_firstAsciiCode >= 65 && d_firstAsciiCode <= 90){
-        d_secondAsciiCode = (d_firstAsciiCode+65 - offset) % 26 + 65;
+        d_secondAsciiCode = (d_firstAsciiCode + 65 - offset) % 26 + 65;
         d_newString = String.fromCharCode(d_secondAsciiCode);
         decipherArray.push(d_newString);
-     } else if(d_firstAsciiCode = 32){
+     } else {
         d_secondAsciiCode = d_firstAsciiCode;
         d_newString = String.fromCharCode(d_secondAsciiCode);
         decipherArray.push(d_newString);
      }
   }
-      return result2.innerHTML = decipherArray.join('')
+      return decipherArray.join('')
   }}
 
+
+
+ 
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 //Primero obtengo el input text y el input button a través de dos variables diferentes
-let string1 = document.getElementById("textToCipherAndDecipher");
-let offsetNumber = document.getElementById("number");
 
-let button1 = document.getElementById("cipherButton");
-let button2 = document.getElementById("decipherButton");
+let d_button = document.getElementById("decipherButton");
 
-//Ahora obtenemos el identificador para la respuesta del mensaje cifrado y descifrado
-let result1 = document.getElementById("cipherResult");
-button1.addEventListener("click", cipher.encode); 
-let result2 = document.getElementById("decipherResult")
-button2.addEventListener("click", cipher.decode); 
+d_button.addEventListener("click", function (){
+    let offset = parseInt(document.getElementById("offset1").value);
+    let string = document.getElementById("textToDecipher").value.toUpperCase();
+    let decode = document.getElementById('decipherResult');
+    let show = document.getElementById('block_2')
+    let hide = document.getElementById('block_1')
+    decode.innerHTML = cipher.decode(offset,string);
+    if(decode) {
+        show.style.display = 'block';
+        hide.style.display = 'none';
+        }
+}); 
+
+let con_button = document.getElementById('confirmButton');
+con_button.addEventListener('click', function () {
+    let show1 = document.getElementById('block_3');
+    let hide1 = document.getElementById('block_1');
+    let hide2 = document.getElementById('block_2');
+        show1.style.display = 'block';
+        hide1.style.diplay = 'none';
+        hide2.style.display = 'none';
+    
+})
 
 
+let c_button = document.getElementById("cipherButton");
 
+c_button.addEventListener("click", function (){
+    let offset = parseInt(document.getElementById("offset2").value);
+    let string = document.getElementById("textToCipher").value.toUpperCase();
+    let encode = document.getElementById("cipherResult");
+    encode.innerHTML = cipher.encode(offset,string);
+}); 
 
+let m_button = document.getElementById('sendMessage');
+m_button.addEventListener('click', function (){
+    alert('Tu mensaje fue enviado, te esperamos!')
+})
 
 
