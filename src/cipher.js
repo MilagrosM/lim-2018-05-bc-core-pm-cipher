85b0ad6255561738820f1377e1e4c85994926e90
MilagrosM <maytacasas21@gmail.com>
2018-06-04 08:20:55 -0500

    proyecto casi terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,51 @@
 window.cipher = {
+// Función de cifrado
   encode: function(offset,string){
-  
-  let cipherArray = [];
-  
-  for(let i = 0; i<string.length; i++){
-     let c_firstAsciiCode = string.charCodeAt(i);
-     let c_secondAsciiCode;
-     let c_newString;
-    if(c_firstAsciiCode >= 65 && c_firstAsciiCode <= 90){
-      c_secondAsciiCode = (c_firstAsciiCode - 65 + offset) % 26 + 65;
-      c_newString = String.fromCharCode(c_secondAsciiCode);
-      cipherArray.push(c_newString);
-      } else {
-      c_secondAsciiCode = c_firstAsciiCode;
-      c_newString = String.fromCharCode(c_secondAsciiCode);
-      cipherArray.push(c_newString);
-     }     
-  }
+    // Se declara un array que almacenara los nuevos caracteres cifrados
+    let cipherArray = [];
+    // Se utiliza el for para hacer recorrido por toda la cadena ingresada  
+      for(let i = 0; i<string.length; i++) {
+        // Se obtiene el código ASCII para cada caracter de la cadena
+        let c_firstAsciiCode = string.charCodeAt(i);
+        let c_secondAsciiCode;
+        let c_newString;
+        // Se utiliza la estructura if para establecer una condición donde se aplicará el algoritmo de cifrado
+        if (c_firstAsciiCode >= 65 && c_firstAsciiCode <= 90) {
+          c_secondAsciiCode = (c_firstAsciiCode - 65 + offset) % 26 + 65;
+          //Se obtiene los nuevos caracteres y se mandan al array
+          c_newString = String.fromCharCode(c_secondAsciiCode);
+          cipherArray.push(c_newString);
+        } else {
+          //Se establece una igualdad para los espacios en blanco
+          c_secondAsciiCode = c_firstAsciiCode;
+          c_newString = String.fromCharCode(c_secondAsciiCode);
+          cipherArray.push(c_newString);
+        }     
+      }
   return cipherArray.join('');
   },
 
-
-  decode: function(offset,string){
- 
-
-  let decipherArray = [];
+// Función de descifrado, similar a la de encode.
+  decode: function(offset,string) {
+    let decipherArray = [];
   
-  for(let i = 0; i<string.length; i++){
-  
-     let d_firstAsciiCode = string.charCodeAt(i);
-     let d_secondAsciiCode;
-     let d_newString;
-     if(d_firstAsciiCode >= 65 && d_firstAsciiCode <= 90){
-        d_secondAsciiCode = (d_firstAsciiCode + 65 - offset) % 26 + 65;
-        d_newString = String.fromCharCode(d_secondAsciiCode);
-        decipherArray.push(d_newString);
-     } else {
-        d_secondAsciiCode = d_firstAsciiCode;
-        d_newString = String.fromCharCode(d_secondAsciiCode);
-        decipherArray.push(d_newString);
-     }
-  }
-      return decipherArray.join('')
+      for(let i = 0; i<string.length; i++) {
+        let d_firstAsciiCode = string.charCodeAt(i);
+        let d_secondAsciiCode;
+        let d_newString;
+        if (d_firstAsciiCode >= 65 && d_firstAsciiCode <= 90) {
+          d_secondAsciiCode = (d_firstAsciiCode + 65 - offset) % 26 + 65;
+          d_newString = String.fromCharCode(d_secondAsciiCode);
+          decipherArray.push(d_newString);
+        } else {
+          d_secondAsciiCode = d_firstAsciiCode;
+          d_newString = String.fromCharCode(d_secondAsciiCode);
+          decipherArray.push(d_newString);
+        }
+      }
+  return decipherArray.join('')
   }}
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-//Primero obtengo el input text y el input button a través de dos variables diferentes
-
-
-let c_button = document.getElementById("cipherButton");
-
-c_button.addEventListener("click", function (){
-    let offset = parseInt(document.getElementById("offset").value);
-    let string = document.getElementById("textToCipherDecipher").value.toUpperCase();
-    let encode = document.getElementById("cipherResult");
+// Eventos del DOM
+// Se obtiene el id del botón de cifrado para luego establecer un escuchador de eventos que al hacer click, lanza la función de cifrado
+let c_button = document.getElementById('cipherButton');
+c_button.addEventListener('click', function () {
+    let offset = parseInt(document.getElementById('offset').value);
+    let string = document.getElementById('textToCipherDecipher').value.toUpperCase();
+    let encode = document.getElementById('cipherResult');
     encode.innerHTML = cipher.encode(offset,string);
 }); 
 
-let d_button = document.getElementById("decipherButton");
-
-d_button.addEventListener("click", function (){
-    let offset = parseInt(document.getElementById("offset").value);
-    let string = document.getElementById("textToCipherDecipher").value.toUpperCase();
+// Se obtiene el id del botón de descifrado para luego establecer un escuchador de eventos que al hacer click, lanza la función de descifrado
+let d_button = document.getElementById('decipherButton');
+d_button.addEventListener('click', function () {
+    let offset = parseInt(document.getElementById('offset').value);
+    let string = document.getElementById('textToCipherDecipher').value.toUpperCase();
     let decode = document.getElementById('decipherResult');
     decode.innerHTML = cipher.decode(offset,string);
 }); 
 
+// Botón para conocer un poco más sobre cifrado cesar
+let know_button = document.getElementById('knowButton');
+let block1 = document.getElementById('block_1');
+let block2 = document.getElementById('block_2');
+know_button.addEventListener('click',function () {
+    block1.style.display = 'none';
+    block2.style.display = 'block'; 
+});
 
-
-
-
+// Botón para regresar
+let back_button = document.getElementById('backButton');
+back_button.addEventListener('click',function () {
+    block1.style.display = 'block';
+    block2.style.display = 'none'; 
+})
